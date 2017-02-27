530839e4dc24f978405b513616224794f8cdce8e
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-02-27 15:16:16 -0800

    WordbotsServer returns an error if parse is not of category S (close #6)

--- src/main/scala/wordbots/WordbotsServer.scala
+++ src/main/scala/wordbots/WordbotsServer.scala
@@ -1,5 +1,6 @@
 package wordbots
 
+import com.workday.montague.ccg.S
 import com.workday.montague.semantics.Form
 import org.http4s._
 import org.http4s.dsl._
@@ -7,7 +8,6 @@ import org.http4s.server.{Server, ServerApp}
 import org.http4s.server.blaze.BlazeBuilder
 
 import scala.util.{Failure, Success}
-
 import scalaz.concurrent.Task
 
 object WordbotsServer extends ServerApp {
@@ -22,10 +22,16 @@ object WordbotsServer extends ServerApp {
         case Some("js") =>
          result.bestParse.map(_.semantic) match {
             case Some(Form(v: AstNode)) =>
-              AstValidator.validate(v) match {
-                case Success(_) => Ok("{\"js\": \"" + CodeGenerator.generateJS(v) + "\"}", headers())
-                case Failure(ex: Throwable) =>
-                  Ok("{\"error\": \"" + ex.getMessage + "\"}", headers())
+              val syntacticCat = result.bestParse.map(_.syntactic
+              syntacticCat match {
+                case Some(S) =>
+                  AstValidator.validate(v) match {
+                    case Success(_) => Ok("{\"js\": \"" + CodeGenerator.generateJS(v) + "\"}", headers())
+                    case Failure(ex: Throwable) =>
+                      Ok("{\"error\": \"" + ex.getMessage + "\"}", headers())
+                  }
+                case _ =>
+                  Ok("{\"error\": \"Parser did not produce a complete sentence - expected category: Some(S), got: " + syntacticCat + "\"}", headers())
               }
             case _ =>
               val unrecognizedTokens = Parser.findUnrecognizedTokens(input)
