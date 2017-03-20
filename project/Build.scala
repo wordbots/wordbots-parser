7a210d74bad1836618b1e5cd38a27f023066abe6
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-03-20 00:51:33 -0700

    as above

--- project/Build.scala
+++ project/Build.scala
@@ -3,5 +3,5 @@ import sbt._
 object MyBuild extends Build {
   lazy val root = Project("root", file(".")) dependsOn montague
 
-  lazy val montague = RootProject(uri("git://github.com/Workday/upshot-montague.git#2599c75"))
+  lazy val montague = RootProject(uri("git://github.com/Workday/upshot-montague.git#b20475f"))
 }
