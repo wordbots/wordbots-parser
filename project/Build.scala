3092ddc44cf57fd4872f6c7e191752acc09e4750
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-05-27 19:08:31 -0700

    up montague version

--- project/Build.scala
+++ project/Build.scala
@@ -3,5 +3,5 @@ import sbt.{Build => SbtBuild, _}
 object Build extends SbtBuild {
   lazy val root = Project("root", file(".")) dependsOn montague
 
-  lazy val montague = RootProject(uri("git://github.com/Workday/upshot-montague.git#385e084"))
+  lazy val montague = RootProject(uri("git://github.com/Workday/upshot-montague.git#0609dfe"))
 }
