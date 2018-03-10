3e3a070d7329a42d6c2108c4e9839dada6fd894c
Michael <mebert@pacbell.net>
2018-03-09 16:04:09 -0800

    Changed most sums to products, as they're much more common and much more likely to be the right thing.

--- src/main/scala/wordbots/CostEstimator.scala
+++ src/main/scala/wordbots/CostEstimator.scala
@@ -5,13 +5,13 @@ package wordbots
 
 object CostEstimator {
   def estimateCost(node: AstNode, mode:Option[String]): String =
-    (baseCost(mode)(genericEstimate(node))).toString
+    {println("\n\n---new cost---");(baseCost(mode)(genericEstimate(node))).toString}
 
   private def baseCost(mode:Option[String]) : Float=>Float ={
     mode match{
-      case Some("Object") => (x => 1 + 1.5f * x)//objects are more expensive than events and have a higher base cost
-      case Some("Event") => (x => 0 + 1 * x)
-      case _ => (x => 1 * x)
+      case Some("Object") => (x => x)
+      case Some("Event") => (x => x)
+      case _ => (x =>  x)
     }
   }
 
@@ -28,54 +28,54 @@ object CostEstimator {
       case Until(TurnsPassed(num), action) => 1 * childCosts(node).sum
 
       // Actions: Normal
-      case BecomeACopy(source, target)    => 1 * childCosts(node).sum
-      case CanAttackAgain(target)         => 1 * childCosts(node).sum
-      case CanMoveAgain(target)           => 1 * childCosts(node).sum
-      case CanMoveAndAttackAgain(target)  => 1 * childCosts(node).sum
+      case BecomeACopy(source, target)    => 1 * childCosts(node).product
+      case CanAttackAgain(target)         => 1 * childCosts(node).product
+      case CanMoveAgain(target)           => 1 * childCosts(node).product
+      case CanMoveAndAttackAgain(target)  => 1 * childCosts(node).product
       case DealDamage(target, num)        => -1 * childCosts(node).product
-      case Destroy(target)                => -2 * childCosts(node).sum
-      case Discard(target)                => -1 * childCosts(node).sum
+      case Destroy(target)                => -2 * childCosts(node).product
+      case Discard(target)                => -1 * childCosts(node).product
       case Draw(target,num)               => 1 * childCosts(node).product
       case EndTurn                        => 1
       case GiveAbility(target, ability)   => 1 * childCosts(node).product
       case ModifyAttribute(target, attr, op)=>1 * childCosts(node).product
       case ModifyEnergy(target, op)       => 1 * childCosts(node).product
-      case MoveObject(target, dest)       => 1 * childCosts(node).sum
+      case MoveObject(target, dest)       => 1 * childCosts(node).product
       case PayEnergy(target, amount)      => -0.5f * childCosts(node).product
-      case RemoveAllAbilities(target)     => 1 * childCosts(node).sum
-      case RestoreAttribute(target, Health, Some(num)) => 1 * childCosts(node).sum
-      case RestoreAttribute(target, Health, None) => 1 * childCosts(node).sum
-      case ReturnToHand(target)           => 1 * childCosts(node).sum
+      case RemoveAllAbilities(target)     => 1 * childCosts(node).product
+      case RestoreAttribute(target, Health, Some(num)) => 1 * childCosts(node).product
+      case RestoreAttribute(target, Health, None) => 1 * childCosts(node).product
+      case ReturnToHand(target)           => 1 * childCosts(node).product
       case SetAttribute(target, attr, num)=> 1 * childCosts(node).product
-      case SwapAttributes(target, attr1, attr2) => 1 * childCosts(node).sum//TODO FIX
+      case SwapAttributes(target, attr1, attr2) => 1 * childCosts(node).product
       case TakeControl(player, target)    => 2 * childCosts(node).product
 
       // Actions: Utility
-      case SaveTarget(target)             => 1 * childCosts(node).sum
+      case SaveTarget(target)             => 1 * childCosts(node).product
 
       // Activated and triggered abilities
-      case ActivatedAbility(action)       => 1 * childCosts(node).sum
-      case TriggeredAbility(trigger, Instead(action)) => 1 * childCosts(node).sum
-      case TriggeredAbility(trigger, action) => 1 * childCosts(node).sum
+      case ActivatedAbility(action)       => 1 * childCosts(node).product
+      case TriggeredAbility(trigger, Instead(action)) => 1 * childCosts(node).product
+      case TriggeredAbility(trigger, action) => 1 * childCosts(node).product
 
       // Passive abilities
-      case ApplyEffect(target, effect)    => 1 * childCosts(node).sum
-      case AttributeAdjustment(target, attr, op) => 1 * childCosts(node).sum
-      case FreezeAttribute(target, attr)  => 1 * childCosts(node).sum
-      case HasAbility(target, ability)    => 1 * childCosts(node).sum
+      case ApplyEffect(target, effect)    => 1 * childCosts(node).product
+      case AttributeAdjustment(target, attr, op) => 1 * childCosts(node).product
+      case FreezeAttribute(target, attr)  => 1 * childCosts(node).product
+      case HasAbility(target, ability)    => 1 * childCosts(node).product
 
       // Effects
-      case CanOnlyAttack(target)          => 1 * childCosts(node).sum
+      case CanOnlyAttack(target)          => 1 * childCosts(node).product
 
       // Triggers
-      case AfterAttack(targetObj, objectType) => 1 * childCosts(node).sum
-      case AfterCardPlay(targetPlayer, cardType) => 1 * childCosts(node).sum
-      case AfterDamageReceived(targetObj) => 1 * childCosts(node).sum
-      case AfterDestroyed(targetObj, cause) => 1 * childCosts(node).sum
-      case AfterMove(targetObj)           => 1 * childCosts(node).sum
-      case AfterPlayed(targetObj)         => 1 * childCosts(node).sum
-      case BeginningOfTurn(targetPlayer)  => 1 * childCosts(node).sum
-      case EndOfTurn(targetPlayer)        => 1 * childCosts(node).sum
+      case AfterAttack(targetObj, objectType) => 1 * childCosts(node).product
+      case AfterCardPlay(targetPlayer, cardType) => 1 * childCosts(node).product
+      case AfterDamageReceived(targetObj) => 1 * childCosts(node).product
+      case AfterDestroyed(targetObj, cause) => 0.8f * childCosts(node).product
+      case AfterMove(targetObj)           => 2 * childCosts(node).product
+      case AfterPlayed(targetObj)         => 1 * childCosts(node).product
+      case BeginningOfTurn(targetPlayer)  => 2 * childCosts(node).product
+      case EndOfTurn(targetPlayer)        => 2 * childCosts(node).product
 
       // Target objects
       case ChooseO(collection)            => 2 * childCosts(node).sum
@@ -97,38 +97,38 @@ object CostEstimator {
       case Self                           => 1
       case Opponent                       => -1
       case AllPlayers                     => 1
-      case ControllerOf(targetObject)     => 1 * childCosts(node).sum
+      case ControllerOf(targetObject)     => 1 * childCosts(node).product
 
       // Conditions
-      case AdjacentTo(obj)                => 1 * childCosts(node).sum
+      case AdjacentTo(obj)                => 1 * childCosts(node).product
       case AttributeComparison(attr, comp)=> 1 * childCosts(node).product
-      case ControlledBy(player)           => 1 * childCosts(node).sum
-      case HasProperty(property)          => 1 * childCosts(node).sum
+      case ControlledBy(player)           => 1 * childCosts(node).product
+      case HasProperty(property)          => 1 * childCosts(node).product
       case Unoccupied                     => 1
       case WithinDistanceOf(distance, obj)=> 1 * childCosts(node).sum
 
       // Global conditions
-      case CollectionExists(coll)         =>  1 * childCosts(node).sum
-      case TargetHasProperty(target, property) => 1 * childCosts(node).sum
+      case CollectionExists(coll)         =>  1 * childCosts(node).product
+      case TargetHasProperty(target, property) => 1 * childCosts(node).product
 
       // Arithmetic operations
-      case Constant(num)                  => 1 * childCosts(node).sum
-      case Plus(num)                      => 1 * childCosts(node).sum
-      case Minus(num)                     => -1 * childCosts(node).sum
-      case Multiply(num)                  => 2 * childCosts(node).sum
-      case Divide(num, RoundedDown)       => 1 * childCosts(node).sum
-      case Divide(num, RoundedUp)         => 1 * childCosts(node).sum
+      case Constant(num)                  => 1 * childCosts(node).product
+      case Plus(num)                      => 1 * childCosts(node).product
+      case Minus(num)                     => -1 * childCosts(node).product
+      case Multiply(num)                  => 2 * childCosts(node).product
+      case Divide(num, RoundedDown)       => 1 * childCosts(node).product
+      case Divide(num, RoundedUp)         => 1 * childCosts(node).product
 
       // Comparisons
-      case EqualTo(num)                   => 0.5f * childCosts(node).sum
-      case GreaterThan(num)               => 1 * childCosts(node).sum
-      case GreaterThanOrEqualTo(num)      => 1 * childCosts(node).sum
-      case LessThan(num)                  => 1 * childCosts(node).sum
-      case LessThanOrEqualTo(num)         => 1 * childCosts(node).sum
+      case EqualTo(num)                   => 0.5f * childCosts(node).product
+      case GreaterThan(num)               => 1 * childCosts(node).product
+      case GreaterThanOrEqualTo(num)      => 1 * childCosts(node).product
+      case LessThan(num)                  => 1 * childCosts(node).product
+      case LessThanOrEqualTo(num)         => 1 * childCosts(node).product
 
 
       // Numbers
-      case Scalar(int)                    => scala.math.pow(childCosts(node).sum,2).toFloat
+      case Scalar(int)                    => scala.math.pow(childCosts(node).product,2).toFloat
       case AttributeSum(collection, attr) => 1 * childCosts(node).sum
       case AttributeValue(obj, attr)      => 1 * childCosts(node).sum
       case Count(collection)              => 1 * childCosts(node).sum
@@ -136,16 +136,16 @@ object CostEstimator {
 
       // Collections
       case AllTiles                       => 1
-      case CardsInHand(player, cardType)  => 1 * childCosts(node).sum
+      case CardsInHand(player, cardType)  => 1 * childCosts(node).product
       case ObjectsMatchingConditions(objType, conditions) => 1 * childCosts(node).product
       case Other(collection)              => 1 * childCosts(node).sum
-      case TilesMatchingConditions(conditions) => 1 * childCosts(node).sum
+      case TilesMatchingConditions(conditions) => 1 * childCosts(node).product
 
       // Labels
       case m: MultiLabel                  => {println("multilabel. what is it?");1}
       case l: Label                       => 1
 
-      case _                              => 1 * childCosts(node).sum
+      case _                              => 1 * childCosts(node).product
     }
   }
 
