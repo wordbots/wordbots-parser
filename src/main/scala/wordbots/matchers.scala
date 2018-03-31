e174f12dcb37b264b59aa360b991c6076236cd27
Alex Nisnevich <alex.nisnevich@gmail.com>
2018-03-31 13:15:33 -0700

    StatsTripleMatcher

--- src/main/scala/wordbots/matchers.scala
+++ src/main/scala/wordbots/matchers.scala
@@ -2,6 +2,8 @@ package wordbots
 
 import com.workday.montague.parser.TokenMatcher
 
+import scala.util.Try
+
 object StrictIntegerMatcher extends TokenMatcher[Int] {
   def apply(str: String): Seq[Int] = {
     if (str forall Character.isDigit) {
@@ -33,6 +35,18 @@ object NumberWordMatcher extends TokenMatcher[Int] {
     numberWords.indexOf(str) match {
       case idx if idx > -1 => Seq(idx)
       case _               => Nil
+    }
+  }
+}
+
+case class StatsTriple(attack: Int, health: Int, speed: Int)
+object StatsTripleMatcher extends TokenMatcher[StatsTriple] {
+  private def stringToOptInt(s: String): Option[Int] = Try(s.toInt).toOption
+
+  def apply(str: String): Seq[StatsTriple] = {
+    str.split("/").map(stringToOptInt) match {
+      case Array(Some(attack), Some(health), Some(speed)) => Seq(StatsTriple(attack, health, speed))
+      case _ => Nil
     }
   }
 }
