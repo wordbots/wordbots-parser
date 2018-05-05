04173169c66693f42ae2164fa6f7e03a5688715f
Alex Nisnevich <alex.nisnevich@gmail.com>
2018-05-05 13:27:52 -0700

    Rework 'and' to parse 'Give a robot with 1 attack and 1 speed and 1 health 1 attack.' (#76)
    * Rework 'and' to parse 'Give a robot with 1 attack and 1 speed and 1 health 1 attack.'
    
    * minor

--- src/main/scala/wordbots/syntax.scala
+++ src/main/scala/wordbots/syntax.scala
@@ -1,6 +1,6 @@
 package wordbots
 
-import com.workday.montague.ccg.TerminalCat
+import com.workday.montague.ccg.{CcgCat, TerminalCat}
 
 case object Adj extends TerminalCat { val category = "Adj" }
 case object Adv extends TerminalCat { val category = "Adv" }
@@ -8,3 +8,13 @@ case object N extends TerminalCat { val category = "Noun" }  // montague treats 
 case object Num extends TerminalCat { val category = "#" }
 case object Quoted extends TerminalCat { val category = "\"" }
 case object V extends TerminalCat { val category = "V" }
+
+// Reverse conjunction: (X/X)\X for any category X (as opposed to regular conjunction, which is (X\X)/X ).
+// This can be useful when you want the arguments to a conjunction to be evaluated from left-to-right.
+// NOTE: Strictly speaking, this is not a terminal category, but wordbots.montague.ccg.CcgCat is a sealed trait.
+// TODO: Either unseal CcgCat or add ReverseConj to montague.
+case object ReverseConj extends TerminalCat {
+  override def getForwardApplication(right: CcgCat): Option[CcgCat] = None
+  override def getBackwardApplication(left: CcgCat): Option[CcgCat] = Some(left/left)
+  val category = "((X/X)\\X)"
+}
