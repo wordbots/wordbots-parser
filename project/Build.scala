541ed9fd90c5f2e7a0255ae21bc78595da5e2856
Alex Nisnevich <alex.nisnevich@gmail.com>
2019-09-01 15:15:35 -0700

    Bump montague version to 583d3d1a8b5e61230aa3c6111fd8ef5365c6514a to fix #166

--- project/Build.scala
+++ project/Build.scala
@@ -3,5 +3,5 @@ import sbt.{Build => SbtBuild, _}
 object Build extends SbtBuild {
   lazy val root = Project("root", file(".")) dependsOn montague
 
-  lazy val montague = RootProject(uri("git://github.com/Workday/upshot-montague.git#0c37d3da966f57050e238cc612d7ddde15aeb37f"))
+  lazy val montague = RootProject(uri("git://github.com/Workday/upshot-montague.git#583d3d1a8b5e61230aa3c6111fd8ef5365c6514a"))
 }
