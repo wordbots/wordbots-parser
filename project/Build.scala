e3054892ada07685becefd7cbaa80dd547af742f
Alex Nisnevich <alex.nisnevich@gmail.com>
2019-09-01 15:22:58 -0700

    fix compilation

--- project/Build.scala
+++ project/Build.scala
@@ -1,7 +1,9 @@
 import sbt.{Build => SbtBuild, _}
 
 object Build extends SbtBuild {
+  val MONTAGUE_COMMIT_SHA = "bc4b7233cf4e7e85e93c2f3f35028a7bbec1e6aa"
+
   lazy val root = Project("root", file(".")) dependsOn montague
 
-  lazy val montague = RootProject(uri("git://github.com/Workday/upshot-montague.git#583d3d1a8b5e61230aa3c6111fd8ef5365c6514a"))
+  lazy val montague = RootProject(uri(s"git://github.com/Workday/upshot-montague.git#$MONTAGUE_COMMIT_SHA"))
 }
